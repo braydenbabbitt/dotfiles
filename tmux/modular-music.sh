2de54a5713dbbe1c67b88d654ac430462d4c5e0d
Brayden Babbitt <1495342+braydenbabbitt@users.noreply.github.com>
2024-12-31 08:40:24 -0700

    fixed other errors

--- tmux/modular-music.sh
+++ tmux/modular-music.sh
@@ -6,12 +6,14 @@ frontend_session_name="modular-music-fe"
 backend_session_name="modular-music-be"
 group_name="modular-music"
 
-tmux new-session -d -s $frontend_session_name -n fe-nvim -A -t $group_name -c $frontend_dir
-tmux send-keys "nvim ."
+tmux new-session -d -s $frontend_session_name -A -t $group_name -c $frontend_dir
+tmux new-window -n fe-nvim
+tmux send-keys "nvim ." C-m
 tmux new-window -n fe-term
 tmux send-keys "npm i && npm run dev" C-m
 
-tmux new-session -d -s $backend_session_name -n be-nvim -A -t $group_name -c $backend_dir
+tmux new-session -d -s $backend_session_name -A -t $group_name -c $backend_dir
+tmux new-window -n be-nvim
 tmux send-keys "nvim ." C-m
 tmux new-window -n be-term -d
 tmux select-window -t fe-nvim
