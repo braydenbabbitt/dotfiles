ced610102b81c50ce1dd69d52248dad40b80914a
Brayden Babbitt <brayden@babbitts.net>
2024-12-31 08:37:52 -0700

    fixed enter key

--- tmux/modular-music.sh
+++ tmux/modular-music.sh
@@ -9,9 +9,9 @@ group_name="modular-music"
 tmux new-session -d -s $frontend_session_name -n fe-nvim -A -t $group_name -c $frontend_dir
 tmux send-keys "nvim ."
 tmux new-window -n fe-term
-tmux send-keys "npm i && npm run dev"
+tmux send-keys "npm i && npm run dev" C-m
 
 tmux new-session -d -s $backend_session_name -n be-nvim -A -t $group_name -c $backend_dir
-tmux send-keys "nvim ."
+tmux send-keys "nvim ." C-m
 tmux new-window -n be-term -d
 tmux select-window -t fe-nvim
