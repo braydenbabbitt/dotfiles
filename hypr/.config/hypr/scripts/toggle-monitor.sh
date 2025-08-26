9868e5a8810d2fca001853433d68183ad4310251
Brayden Babbitt <1495342+braydenbabbitt@users.noreply.github.com>
2025-08-26 14:41:18 -0600

    fixed monitor toggle

--- hypr/.config/hypr/scripts/toggle-monitor.sh
+++ hypr/.config/hypr/scripts/toggle-monitor.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-MONITOR="DP-3"
+MONITOR="HDMI-A-1"
+MONITOR_CONFIG="HDMI-A-1,2560x1440@144,0x0,1"
 
 if hyprctl monitors | grep -q "^Monitor $MONITOR"; then
   hyprctl keyword monitor "$MONITOR,disable"
   notify-send "Monitor" "$MONITOR disabled"
 else
-  hyprctl keyword monitor "$MONITOR,3840x1600,1920x-200,1"
+  hyprctl keyword monitor "$MONITOR_CONFIG"
   notify-send "Monitor" "$MONITOR enabled"
 fi
